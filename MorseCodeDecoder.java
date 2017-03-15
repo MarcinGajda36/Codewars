bbc38468c800665e6699bf154f2a3967f83c1316
MarcinGajda36 <kirgo36@gmail.com>
2017-03-15 18:13:46 +0100

    added comment
    //4yu

--- MorseCodeDecoder.java
+++ MorseCodeDecoder.java
@@ -1,7 +1,7 @@
 /// assertThat(MorseCodeDecoder.decodeMorse(MorseCodeDecoder.decodeBits("1100110011001100000011000000111111001100111111001111110000000000000011001111110011111100111111000000110011001111110000001111110011001100000011")),
 // is("HEY JUDE"));
 
-public class MorseCodeDecoder {
+public class MorseCodeDecoder {//4kyu
 	public static String SOS = "...---...";
 
 	// [0] = template, [1] = length, [2] = meaning
